75b1704f7bf73d15c30b9d478ea1fb4f59e960a5
brunatholl <bruna.tholl.ti@gmail.com>
2024-08-26 21:57:59 -0300

    ok

--- api/geradorboletim/getDirs.php
+++ api/geradorboletim/getDirs.php
@@ -9,7 +9,7 @@ if (file_exists($dir)) {
         while (false !== ($file = readdir($handle))) {
             if ($file != "." && $file != "..") {
                 $nameFile = explode(".", $file);
-                $file = utf8_encode($file);
+                // $file = utf8_encode($file);
                 if ((count($nameFile) > 1) && ($nameFile[1] == "csv")) {
                     array_push($arrFiles, str_replace("../", "", $dir) . "/" . $file);
                 }
