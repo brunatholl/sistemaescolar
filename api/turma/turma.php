bd00dde184839300c7e22facc0b0377d5b387c3f
brunatholl <bruna.tholl.ti@gmail.com>
2024-08-28 21:40:07 -0300

    ok

--- api/turma/turma.php
+++ api/turma/turma.php
@@ -1,7 +1,7 @@
 <?php
 require_once("../core/header.php");
 
-function getComboEscola(){
+function getComboEscola($codigoEscola = false){
     $arDadosEscolas = array();
     $dadosEscolas = @file_get_contents("../escola/escola.json");
     if($dadosEscolas){
@@ -11,25 +11,34 @@ function getComboEscola(){
 
     $html = '<div style="display:flex;gap:5px;flex-direction:row;">';
 
-    $html .= '  <label for="escola">Escola 10:</label>';
+    $html .= '  <label for="escola">Escola:</label>';
     $html .= '  <select id="escola" name="escola">';
     $html .= '    <option value="0">Selecione</option>';
 
     //  preencher com php - mais options de ESCOLA
     foreach($arDadosEscolas as $aDados){
-        $html .= '<option value="'. $aDados["codigo"] .'">' . $aDados["descricao"] . '</option>';
+        $selected = "";
+        if($codigoEscola == $aDados["codigo"]){
+            $selected = " selected ";                    
+        }
+
+        $html .= '<option value="'. $aDados["codigo"] .'" ' . $selected .'>' . $aDados["descricao"] . '</option>';
     }
 
     $html .= '</select>';
-
     $html .= '</div>';
 
     return $html;
 }
 
 function getDadosTurma($codigoTurmaAlterar){
+    $codigo = "";
+    $escola = "";
     $nome = "";
-    $email = "";
+    $datainicio = "";
+    $datafim = "";
+    $status = "";
+    $periodo = "";
 
     $dadosTurmas = @file_get_contents("turmas.json");
     $arDadosTurmas = json_decode($dadosTurmas, true);
@@ -38,18 +47,31 @@ function getDadosTurma($codigoTurmaAlterar){
     foreach($arDadosTurmas as $aDados){
         $codigoAtual = $aDados["codigo"];
         if($codigoTurmaAlterar == $codigoAtual){
-            $encontrouTurma = true;
-            $nome = $aDados["nome"];
-            $email = $aDados["email"];
+            $encontrouTurma = true;            
+            $escola       = $aDados["escola"];
+            $nome         = $aDados["nome"];
+            $datainicio   = $aDados["datainicio"];
+            $datafim      = $aDados["datafim"];
+            $status       = $aDados["statuscurso"];
+            $periodo      = $aDados["periodocurso"];
             break;
         }
     }
 
-    return array($nome, $email, $encontrouTurma);
+    return array(
+        $codigoTurmaAlterar,
+        $escola,
+        $nome,
+        $datainicio,
+        $datafim,
+        $status,
+        $periodo, 
+        $encontrouTurma
+    );
 }
 
 // Verificar se existe acao
-$codigo = "";
+$codigoTurma = "";
 $escola = "";
 $nome = "";
 $datainicio = "";
@@ -69,6 +91,7 @@ $selected_status_periodo_1 = "";
 $selected_status_periodo_2 = "";
 $selected_status_periodo_3 = "";
 
+$codigoEscola = false;
 $acaoFormulario = "INCLUIR";
 if(isset($_GET["ACAO"])){
     $acao = $_GET["ACAO"];
@@ -76,10 +99,40 @@ if(isset($_GET["ACAO"])){
         $acaoFormulario = "ALTERAR";
 
         $display = "none;";
-        $codigo = $_GET["codigo"];
-        list($nome, $email, $encontrouTurma) = getDadosTurma($codigo);
+        $codigoTurma = $_GET["codigo"];
+        
+        list($codigoTurma,
+            $escola,
+            $nome,
+            $datainicio,
+            $datafim,
+            $status,
+            $periodo, 
+            $encontrouTurma) = getDadosTurma($codigoTurma);
+
+            if($status == "ABERTO"){
+                $selected_status_curso_1 = " selected ";
+            }            
+            if($status == "ANDAMENTO"){
+                $selected_status_curso_2 = " selected ";
+            }
+            if($status == "CONCLUIDO"){
+                $selected_status_curso_3 = " selected ";
+            }
+
+            if($periodo == "Matutino"){
+                $selected_status_periodo_1 = " selected ";
+            }            
+            if($periodo == "Vespertino"){
+                $selected_status_periodo_2 = " selected ";
+            }
+            if($periodo == "Noturno"){
+                $selected_status_periodo_3 = " selected ";
+            }
 
         if($encontrouTurma){
+            $codigoEscola = $escola;
+
             // Limpa a mensagem de erro
             $mensagemTurmaNaoEncontrado = "";
         } else {
@@ -90,18 +143,9 @@ if(isset($_GET["ACAO"])){
     }
 }
 
-$sHTML = '<div> <link rel="stylesheet" href="../css/formulario.css">';
+$sHTML = '<div><link rel="stylesheet" href="../css/formulario.css">';
 
-$codigo = "";
-$escola = ""; // COMBO DE ESCOLA DO ARQUIVO "ESCOLA.JSON"
-$nome = "";
-$datainicio = "";
-$datafim = "";
-
-$status = ""; // combo de status - fixo
-$periodo = "";// combo de periodo - fixo
-
-$comboEscola = getComboEscola();
+$comboEscola = getComboEscola($codigoEscola);
 
 // FORMULARIO DE CADASTRO DE TURMAS
 $sHTML .= '<h2 style="text-align:center;">Formulário de Turma</h2>
@@ -110,13 +154,12 @@ $sHTML .= '<h2 style="text-align:center;">Formulário de Turma</h2>
         <input type="hidden" id="ACAO" name="ACAO" value="' . $acaoFormulario . '">
 
         <label for="codigo">Código:</label>
-        <input type="hidden" id="codigo" name="codigo" value="' . $codigo . '" required>
-        <input type="text" id="codigoTela" name="codigoTela" value="' . $codigo . '" disabled>
-
+        <input type="hidden" id="codigo" name="codigo" value="' . $codigoTurma . '" required>
+        <input type="text" id="codigoTela" name="codigoTela" value="' . $codigoTurma . '" disabled>
+        
         ' . $comboEscola . '
-
+        
         <br>
-
         <label for="nome">Nome:</label>
         <input type="text" id="nome" name="nome" required value="' . $nome . '">
 
@@ -130,9 +173,8 @@ $sHTML .= '<h2 style="text-align:center;">Formulário de Turma</h2>
                 <input type="date" id="datafim" name="datafim" required value="' . $datafim . '">
             </div>
         </div>
-
         <br>
-        <div style="display:flex;justify-content:space-between;">
+        <div style="display:flex;flex-direction:row;justify-content:space-between;">
             <div style="display:flex;flex-direction:column;">
                 <label for="statuscurso">Status da Turma:</label>
                 <select id="statuscurso" name="statuscurso">
